3be9229a62cde9ab588bee72ae2829df43663951
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2026-04-29 16:07:45 +0300

    Добавить модель и сервис для работы с Event

---
+++ TicketFlow/Services/EventService.cs
@@ -0,0 +1,47 @@
+﻿using TicketFlow.Models;
+
+namespace TicketFlow.Services
+{
+    public class EventService : IEventService
+    {
+        private readonly List<Event> _events = [];
+
+        public List<Event> GetEvents()
+        {
+            return [.. _events];
+        }
+
+        public Event? GetEvent(Guid eventId)
+        {
+            return _events.FirstOrDefault(e => e.Id == eventId);
+        }
+
+        public Guid AddEvent(Event newEvent)
+        {
+            newEvent.Id = Guid.NewGuid();
+            _events.Add(newEvent);
+            return newEvent.Id;
+        }
+
+        public Event? UpdateEvent(Event updatedEvent)
+        {
+            var existingEvent = GetEvent(updatedEvent.Id);
+            if (existingEvent == null)
+                return null;
+
+            existingEvent.Title = updatedEvent.Title;
+            existingEvent.Description = updatedEvent.Description;
+            existingEvent.StartAt = updatedEvent.StartAt;
+            existingEvent.EndAt = updatedEvent.EndAt;
+
+            return existingEvent;
+        }
+
+        public void RemoveEvent(Guid eventId)
+        {
+            var eventToRemove = GetEvent(eventId);
+            if (eventToRemove != null)
+                _events.Remove(eventToRemove);
+        }
+    }
+}
---
+++ TicketFlow/Services/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using TicketFlow.Models;
+
+namespace TicketFlow.Services
+{
+    public interface IEventService
+    {
+        List<Event> GetEvents();
+        Event? GetEvent(Guid eventId);
+        Guid AddEvent(Event newEvent);
+        Event? UpdateEvent(Event updatedEvent);
+        void RemoveEvent(Guid eventId);
+    }
+}
