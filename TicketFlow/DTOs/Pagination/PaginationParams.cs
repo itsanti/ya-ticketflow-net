a0724272097ee688ed0211462b434c6569eb9c60
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2026-05-11 23:36:22 +0300

    Добавить в PaginationParams валидацию параметров page и pageSize

--- TicketFlow/DTOs/Pagination/PaginationParams.cs
+++ TicketFlow/DTOs/Pagination/PaginationParams.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace TicketFlow.DTOs.Pagination
 {
     public class PaginationParams
     {
         [FromQuery(Name = "page")]
+        [Range(1, int.MaxValue, ErrorMessage = "Page number must be greater than 0.")]
         public int Page { get; set; } = 1;
 
         [FromQuery(Name = "pageSize")]
+        [Range(1, 100, ErrorMessage = "Page size must be between 1 and 100.")]
         public int PageSize { get; set; } = 10;
     }
 }
