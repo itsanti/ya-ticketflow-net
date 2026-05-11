9206ac0dc600266149f12c801be624a1f9d1d14d
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2026-05-11 22:58:54 +0300

    Добавить единообразие ошибок для автоматической валидации .NET

--- TicketFlow/Program.cs
+++ TicketFlow/Program.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Mvc;
 using TicketFlow.Middlewares;
 using TicketFlow.Services;
 
@@ -13,7 +14,25 @@ namespace TicketFlow
             // Add services to the container.
             builder.Services.AddSingleton<IEventService, EventService>();
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .ConfigureApiBehaviorOptions(options =>
+                {
+                    options.InvalidModelStateResponseFactory = context =>
+                    {
+                        var errors = context.ModelState.Values
+                            .SelectMany(v => v.Errors)
+                            .Select(e => e.ErrorMessage);
+                        var detailMessage = string.Join(" ", errors);
+                        var problemDetails = new ProblemDetails
+                        {
+                            Status = StatusCodes.Status400BadRequest,
+                            Title = "Validation error",
+                            Detail = detailMessage
+                        };
+
+                        return new BadRequestObjectResult(problemDetails);
+                    };
+                });
 
             builder.Services.AddProblemDetails();
 
