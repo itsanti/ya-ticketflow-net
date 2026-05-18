9a5d9128583c2fe914f7c1c3b2f90c4577baa82b
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2026-05-18 16:30:20 +0300

    Добавить модель Booking

---
+++ TicketFlow/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿namespace TicketFlow.Models
+{
+    public class Booking(Guid eventId)
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid EventId { get; set; } = eventId;
+
+        public BookingStatus Status { get; set; } = BookingStatus.Pending;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? ProcessedAt { get; set; }
+
+    }
+}
