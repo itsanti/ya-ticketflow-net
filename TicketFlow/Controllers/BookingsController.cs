3d972630bb7223fc31ac48d4383d737d19de6217
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2026-05-24 20:39:56 +0300

    Удалить зависимость контроллера

--- TicketFlow/Controllers/BookingsController.cs
+++ TicketFlow/Controllers/BookingsController.cs
@@ -10,7 +10,7 @@ namespace TicketFlow.Controllers
     {
         private readonly IBookingService _bookingService;
 
-        public BookingsController(IBookingService bookingService, IEventService eventService)
+        public BookingsController(IBookingService bookingService)
         {
             _bookingService = bookingService;
         }
