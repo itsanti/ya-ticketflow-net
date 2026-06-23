ca9ff9193142bb4519a0e031351ae3134b7f58a5
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2026-06-23 17:30:13 +0300

    Запустить приложение с БД

--- TicketFlow/Program.cs
+++ TicketFlow/Program.cs
@@ -54,6 +54,12 @@ namespace TicketFlow
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.EnsureCreated();
+            }
+
             app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
             // Configure the HTTP request pipeline.
